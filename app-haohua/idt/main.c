be938ebc7dfac0a7bab683e331035a366ff7496d
randoruf <masswie22@gmail.com>
2023-05-05 07:37:15 +0800

    simple idt

--- app-haohua/idt/main.c
+++ app-haohua/idt/main.c
@@ -33,10 +33,46 @@
 #define NEMESIS_HIGH                1
 
 /* ------------------------------------------------------------ */
+/* This code may execute with ring0 privileges */
+int my_cpl = -1;
+uint64_t my_flags = 0;
+extern uint64_t nemesis_tsc_aex, nemesis_tsc_eresume;
+
+void pre_irq(void)
+{
+    my_cpl = get_cpl();
+    my_flags = read_flags();
+    __ss_irq_fired = 0;
+    nemesis_tsc_eresume = rdtsc_begin();
+}
+
+void do_irq_sw(void)
+{
+    pre_irq();
+    asm("int %0\n\t" ::"i"(IRQ_VECTOR):);
+}
 
 void do_irq_tmr(void)
 {
+    pre_irq();
     apic_timer_irq(10);
+
+    /*
+     * Ring-0 `exec_priv` handler executes with interrupts disabled FLAGS.IF=0
+     * (as the kernel may freeze when interrupting a ring-0 trap gate), so the
+     * APIC timer IRQ should be handled after returning.
+     */
+    if (!(my_flags & (0x1 << 9)))
+        return;
+
+    while (!__ss_irq_fired)
+    {
+        #if NEMESIS_HIGH
+            asm("rdrand %rax\n\t");
+        #else
+            asm("nop\n\t");
+        #endif
+    }
 }
 
 /* ------------------------------------------------------------ */
@@ -50,6 +86,28 @@ void post_irq(char *s)
 
 void do_irq_test(int do_exec_priv)
 {
+    #if DO_APIC_SW_IRQ
+        printf("\n");
+        info_event("Triggering ring-3 software interrupts..");
+        for (int i=0; i < NUM; i++)
+        {
+            do_irq_sw();
+            post_irq("software");
+        }
+
+        if (do_exec_priv)
+        {
+            printf("\n");
+            info_event("Triggering ring-0 software interrupts..");
+            for (int i=0; i < NUM; i++)
+            {
+                my_cpl = -1;
+                exec_priv(do_irq_sw);
+                post_irq("software");
+            }
+        }
+    #endif
+
     #if DO_APIC_TMR_IRQ
         printf("\n");
         info_event("Triggering ring-3 APIC timer interrupts..");
@@ -58,12 +116,20 @@ void do_irq_test(int do_exec_priv)
         for (int i=0; i < NUM; i++)
         {
             do_irq_tmr();
-            while (!__ss_irq_fired)
-            {
-                __asm__("nop\n\t");
-            }
-            __ss_irq_fired = 0; 
             post_irq("APIC timer");
+        }
+
+        if (do_exec_priv)
+        {
+            printf("\n");
+            info_event("Triggering ring-0 APIC timer interrupts..");
+            for (int i=0; i < NUM; i++)
+            {
+                my_cpl = -1;
+                exec_priv(do_irq_tmr);
+                while (!__ss_irq_fired);
+                post_irq("APIC timer");
+            }
         }
         apic_timer_deadline();
     #endif
@@ -73,9 +139,6 @@ int main( int argc, char **argv )
 {
     idt_t idt = {0};
     ASSERT( !claim_cpu(VICTIM_CPU) );
-    ASSERT( !prepare_system_for_benchmark(PSTATE_PCT) );
-    print_system_settings();
-    
     map_idt(&idt);
 
     #if 0
@@ -96,7 +159,7 @@ int main( int argc, char **argv )
     //#if INFINITE_LOOP
     //while(1)
     //#endif
-    do_irq_test(/*do_exec_priv=*/ DO_EXEC_PRIV);
+        do_irq_test(/*do_exec_priv=*/ DO_EXEC_PRIV);
 
     info("all is well; irq_count=%d; exiting..", __ss_irq_count);
     return 0;
